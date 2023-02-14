efcbd8751e447937682e25e277f1d6913a456860
AbaevB <abaev70@hotmail.com>
2023-02-14 16:20:54 +0700

    feat(js) add paintBlock
    add function paintBlock

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-    let colorInput = document.querySelector('.color-input');
-    let colorTextInput = document.querySelector('.text-color-input');
-    let colorBlock = document.querySelector('.color-block');
-    let clearButton = document.querySelector('.clear-btn');
+  let colorInput = document.querySelector('.color-input');
+  let colorTextInput = document.querySelector('.text-color-input');
+  let colorBlock = document.querySelector('.color-block');
+  let clearButton = document.querySelector('.clear-btn');
+
+  /**Окрашивание
+   * блока по вводу значения
+   */
+
+  function paintBlock() {
+    colorBlock.style.backgroundColor = colorInput.value;
+
+  }
 
 
+  colorInput.addEventListener('input', paintBlock);
+
+  // Вызов функций
+  paintBlock();
 });
