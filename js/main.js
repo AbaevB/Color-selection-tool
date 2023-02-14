66d1d447c51702a691a2d2d5a0b9e21f71cbce44
AbaevB <abaev70@hotmail.com>
2023-02-14 16:07:32 +0700

    feat(js): vars
    add variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    let colorInput = document.querySelector('.color-input');
+    let colorTextInput = document.querySelector('.text-color-input');
+    let colorBlock = document.querySelector('.color-block');
+    let clearButton = document.querySelector('.clear-btn');
+
+
 });
